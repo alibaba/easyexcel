15cdbcdc7191cf96562b9d6ef50ffe1d755eb4ce
Muscleape <muscleape529@gmail.com>
2019-04-25 10:07:40 +0800

    清除空行

--- src/main/java/com/alibaba/excel/write/ExcelBuilderImpl.java
+++ src/main/java/com/alibaba/excel/write/ExcelBuilderImpl.java
@@ -113,22 +113,15 @@ public class ExcelBuilderImpl implements ExcelBuilder {
         int i = 0;
         BeanMap beanMap = BeanMap.create(oneRowData);
         for (ExcelColumnProperty excelHeadProperty : context.getExcelHeadProperty().getColumnPropertyList()) {
-
             BaseRowModel baseRowModel = (BaseRowModel) oneRowData;
-
             String cellValue = TypeUtil.getFieldStringValue(beanMap, excelHeadProperty.getField().getName(), excelHeadProperty.getFormat(), excelHeadProperty.getKeyValue());
-
             CellStyle cellStyle = baseRowModel.getStyle(i) != null ? baseRowModel.getStyle(i) : context.getCurrentContentStyle();
-
             Cell cell = WorkBookUtil.createCell(row, i, cellStyle, cellValue, TypeUtil.isNum(excelHeadProperty.getField()), TypeUtil.isEmptyJsonObject(excelHeadProperty.getKeyValue()));
-
             if (null != context.getAfterWriteHandler()) {
                 context.getAfterWriteHandler().cell(i, cell);
             }
-
             i++;
         }
-
     }
 
     private void addOneRowOfDataToExcel(Object oneRowData, int n) {
