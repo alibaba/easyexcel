4df987f9d6100a98c1f1fefae97c0f01884e5518
Jiaju Zhuang <zhuangjiaju@qq.com>
2019-09-27 19:49:45 +0800

    优化07版超大文件读取方案

--- src/test/java/com/alibaba/easyexcel/test/temp/large/TempLargeDataTest.java
+++ src/test/java/com/alibaba/easyexcel/test/temp/large/TempLargeDataTest.java
@@ -2,6 +2,7 @@ package com.alibaba.easyexcel.test.temp.large;
 
 import java.io.FileInputStream;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ import com.alibaba.excel.EasyExcel;
  *
  * @author Jiaju Zhuang
  */
+@Ignore
 public class TempLargeDataTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(LargeDataTest.class);
 
