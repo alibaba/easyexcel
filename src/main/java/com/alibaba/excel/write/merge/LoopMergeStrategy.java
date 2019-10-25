6ecdb90abdabc19a299399ced7db544d8049bd47
zl <zl@bestarschool.com>
2019-10-25 15:07:37 +0800

    输出时合并单元格增加行合并选项

--- src/main/java/com/alibaba/excel/write/merge/LoopMergeStrategy.java
+++ src/main/java/com/alibaba/excel/write/merge/LoopMergeStrategy.java
@@ -13,24 +13,39 @@ import com.alibaba.excel.metadata.Head;
  */
 public class LoopMergeStrategy extends AbstractMergeStrategy {
     private int eachRow;
+    private int columnCount;
     private int columnIndex;
 
     public LoopMergeStrategy(int eachRow, int columnIndex) {
+        this(eachRow, 1, columnIndex);
+    }
+
+    public LoopMergeStrategy(int eachRow, int columnCount, int columnIndex) {
         if (eachRow < 1) {
             throw new IllegalArgumentException("EachRows must be greater than 1");
+        }
+        if (columnCount < 1) {
+            throw new IllegalArgumentException("ColumnCount must be greater than 1");
+        }
+        if (columnCount == 1 && eachRow == 1) {
+            throw new IllegalArgumentException("ColumnCount or eachRows must be greater than 1");
         }
         if (columnIndex < 0) {
             throw new IllegalArgumentException("ColumnIndex must be greater than 0");
         }
         this.eachRow = eachRow;
+        this.columnCount = columnCount;
         this.columnIndex = columnIndex;
     }
 
     @Override
     protected void merge(Sheet sheet, Cell cell, Head head, int relativeRowIndex) {
         if (head.getColumnIndex() == columnIndex && relativeRowIndex % eachRow == 0) {
-            CellRangeAddress cellRangeAddress = new CellRangeAddress(cell.getRowIndex(),
-                cell.getRowIndex() + eachRow - 1, cell.getColumnIndex(), cell.getColumnIndex());
+            CellRangeAddress cellRangeAddress = new CellRangeAddress(
+                cell.getRowIndex(),
+                cell.getRowIndex() + eachRow - 1,
+                cell.getColumnIndex(),
+                cell.getColumnIndex() + columnCount - 1);
             sheet.addMergedRegion(cellRangeAddress);
         }
     }
