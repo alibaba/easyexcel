b3d9147f8e6c32ec8389e8ced3dea509924bbab9
Jiaju Zhuang <zhuangjiaju@qq.com>
2020-03-17 01:37:27 +0800

    修改重复写描述

--- src/test/java/com/alibaba/easyexcel/test/demo/write/WriteTest.java
+++ src/test/java/com/alibaba/easyexcel/test/demo/write/WriteTest.java
@@ -175,8 +175,8 @@ public class WriteTest {
         excelWriter = EasyExcel.write(fileName).build();
         // 去调用写入,这里我调用了五次，实际使用时根据数据库分页的总的页数来。这里最终会写到5个sheet里面
         for (int i = 0; i < 5; i++) {
-            // 每次都要创建writeSheet 这里注意必须指定sheetNo。这里注意DemoData.class 可以每次都变，我这里为了方便 所以用的同一个class 实际上可以一直变
-            writeSheet = EasyExcel.writerSheet(i, "模板").head(DemoData.class).build();
+            // 每次都要创建writeSheet 这里注意必须指定sheetNo 而且sheetName必须不一样。这里注意DemoData.class 可以每次都变，我这里为了方便 所以用的同一个class 实际上可以一直变
+            writeSheet = EasyExcel.writerSheet(i, "模板" + i).head(DemoData.class).build();
             // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
             List<DemoData> data = data();
             excelWriter.write(data, writeSheet);
