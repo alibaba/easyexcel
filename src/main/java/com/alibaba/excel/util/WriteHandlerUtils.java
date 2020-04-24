9a36675e071767a57c064df824738835b2d1d6d9
Jiaju Zhuang <zhuangjiaju@qq.com>
2020-04-24 22:08:09 +0800

    修改`sheet`事件未调用的bug

--- src/main/java/com/alibaba/excel/util/WriteHandlerUtils.java
+++ src/main/java/com/alibaba/excel/util/WriteHandlerUtils.java
@@ -234,6 +234,6 @@ public class WriteHandlerUtils {
         } else {
             writeHandlerMap = writeContext.currentWriteHolder().writeHandlerMap();
         }
-        return writeHandlerMap.get(WorkbookWriteHandler.class);
+        return writeHandlerMap.get(clazz);
     }
 }
