0d83d0c31631c9629e81f1190b03c57cbf55bd37
Hey20 <he@abc.com>
2020-12-22 23:53:32 +0800

    Auto adjust the column width

--- src/test/java/com/alibaba/easyexcel/test/demo/write/WriteTest.java
+++ src/test/java/com/alibaba/easyexcel/test/demo/write/WriteTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
