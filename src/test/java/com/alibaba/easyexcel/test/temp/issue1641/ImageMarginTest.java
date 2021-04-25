b4934980fb3964338a151d8455a64b0a25043b62
wjq427 <58835896+wjq427@users.noreply.github.com>
2021-04-25 11:55:56 +0800

    Update ImageMarginTest.java

--- src/test/java/com/alibaba/easyexcel/test/temp/issue1641/ImageMarginTest.java
+++ src/test/java/com/alibaba/easyexcel/test/temp/issue1641/ImageMarginTest.java
@@ -1,7 +1,5 @@
 package com.alibaba.easyexcel.test.temp.issue1641;
 
-import com.alibaba.easyexcel.test.demo.write.ImageData;
-import com.alibaba.easyexcel.test.demo.write.MultiImageData;
 import com.alibaba.easyexcel.test.util.TestFileUtil;
 import com.alibaba.excel.EasyExcel;
 import org.junit.Test;
@@ -19,14 +17,14 @@ public class ImageMarginTest {
     public void MutiImageWrite() {
         String fileName = TestFileUtil.getPath() + "MutiImageWrite" + System.currentTimeMillis() + ".xlsx";
         List<ImageData> list = new ArrayList<ImageData>();
-        com.alibaba.easyexcel.test.demo.write.ImageData imageData = new com.alibaba.easyexcel.test.demo.write.ImageData();
-        com.alibaba.easyexcel.test.demo.write.ImageData imageData2 = new com.alibaba.easyexcel.test.demo.write.ImageData();
+        ImageData imageData = new ImageData();
+        ImageData imageData2 = new ImageData();
         list.add(imageData);
         list.add(imageData2);
         String imagePath = "src/test/java/com/alibaba/easyexcel/test/temp/issue1641/2.jpg";
         String imagePath2 = "src/test/java/com/alibaba/easyexcel/test/temp/issue1641/img.png";
         imageData.setFile(new File(imagePath));
         imageData2.setFile(new File(imagePath2));
-        EasyExcel.write(fileName, MultiImageData.class).sheet().doWrite(list);
+        EasyExcel.write(fileName, ImageData.class).sheet().doWrite(list);
     }
 }
