75741bf2d880c6734ab978c14ecd638b3428b5be
KID0031 <729133573@qq.com>
2021-05-07 14:37:42 +0800

    back to origin

--- src/main/java/com/alibaba/excel/converters/DefaultConverterLoader.java
+++ src/main/java/com/alibaba/excel/converters/DefaultConverterLoader.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import com.alibaba.excel.converters.bigdecimal.BigDecimalBooleanConverter;
 import com.alibaba.excel.converters.bigdecimal.BigDecimalNumberConverter;
 import com.alibaba.excel.converters.bigdecimal.BigDecimalStringConverter;
-import com.alibaba.excel.converters.biginteger.BigIntegerStringConverter;
 import com.alibaba.excel.converters.booleanconverter.BooleanBooleanConverter;
 import com.alibaba.excel.converters.booleanconverter.BooleanNumberConverter;
 import com.alibaba.excel.converters.booleanconverter.BooleanStringConverter;
@@ -95,8 +94,6 @@ public class DefaultConverterLoader {
         putAllConverter(new StringNumberConverter());
         putAllConverter(new StringStringConverter());
         putAllConverter(new StringErrorConverter());
-
-        putAllConverter(new BigIntegerStringConverter());
     }
 
     private static void initDefaultWriteConverter() {
@@ -116,7 +113,6 @@ public class DefaultConverterLoader {
         putWriteConverter(new ByteArrayImageConverter());
         putWriteConverter(new BoxingByteArrayImageConverter());
         putWriteConverter(new UrlImageConverter());
-        putWriteConverter(new BigIntegerStringConverter());
     }
 
     /**
