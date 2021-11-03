c5ec482bfcac227ef08c7e2405a4ca2b70029bb5
Jiaju Zhuang <zhuangjiaju@qq.com>
2021-11-03 15:03:21 +0800

    修改util授权

--- src/main/java/com/alibaba/excel/util/FileUtils.java
+++ src/main/java/com/alibaba/excel/util/FileUtils.java
@@ -16,8 +16,20 @@ import org.apache.poi.util.DefaultTempFileCreationStrategy;
 import org.apache.poi.util.TempFile;
 
 /**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- * @author jipengfei
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Apache Software Foundation (ASF)
  */
 public class FileUtils {
     public static final String POI_FILES = "poifiles";
