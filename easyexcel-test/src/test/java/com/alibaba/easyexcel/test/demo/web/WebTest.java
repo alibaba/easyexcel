3598da95d0b6dab1a29df37d2821ef5a6f8c585a
nieyingping <nieyingping@alphadata.com.cn>
2022-06-16 10:37:50 +0800

    文件上传测试案例的一些修改

--- easyexcel-test/src/test/java/com/alibaba/easyexcel/test/demo/web/WebTest.java
+++ easyexcel-test/src/test/java/com/alibaba/easyexcel/test/demo/web/WebTest.java
@@ -17,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
